0f24c18eac9e6ce8811117d27db6640bcfd61fde
manabpokhrel7 <65856123+manabpokhrel7@users.noreply.github.com>
2026-05-01 16:52:06 -0400

    Added cilium

--- TerraformVM/output.sh
+++ TerraformVM/output.sh
@@ -7,6 +7,6 @@ echo "[control]" >> ../inventory
 awk -F'"' 'NR==1 { print $2 }' output.txt >> ../inventory
 
 echo "[workers]" >> ../inventory
-awk -F'"' 'NR==2 { print $2 }' output.txt >> ../inventory
 awk -F'"' 'NR==3 { print $2 }' output.txt >> ../inventory
+awk -F'"' 'NR==4 { print $2 }' output.txt >> ../inventory
 
