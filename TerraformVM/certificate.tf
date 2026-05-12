a33a2025299b990b68975ae54babd40ceb29bac8
manabpokhrel7 <65856123+manabpokhrel7@users.noreply.github.com>
2026-05-11 21:05:10 -0400

    CICD

--- TerraformVM/certificate.tf
+++ TerraformVM/certificate.tf
@@ -2,6 +2,11 @@ resource "google_compute_managed_ssl_certificate" "kubernetes_cert" {
   name = "manab-kubernetes-managed-cert"
 
   managed {
-    domains = ["kubernetes.manabpokhrel.com.np"]
+    domains = [
+      "kubernetes.manabpokhrel.com.np",
+      "kibana.manabpokhrel.com.np",
+      "grafana.manabpokhrel.com.np",
+      "prometheus.manabpokhrel.com.np"
+    ]
   }
 }
