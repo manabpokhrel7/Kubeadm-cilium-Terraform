db1ca2413008abee61c9ecda4b92a20e5ceea5ad
manabpokhrel7 <65856123+manabpokhrel7@users.noreply.github.com>
2026-05-19 15:52:15 -0400

    Test app

--- control_join.sh
+++ control_join.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-kubeadm join api.kubernetes.manabpokhrel.com.np:6443 --token mimpir.135rfh9ju3gndxky --discovery-token-ca-cert-hash sha256:07aee342846e393323ba8b39cc5960d4cce15e8b07c8edd1a4d9c9db41c90661  --control-plane --certificate-key cedcdb4b5b573cba3c6428be70d7897137a6ff63bf71d66fa179b2dd4f988616
+kubeadm join api.kubernetes.manabpokhrel.com.np:6443 --token bao02u.8vyt30zvq88e57cz --discovery-token-ca-cert-hash sha256:1dd62d5bc25c6b90977ce21a3932317331f2b917374b85cfd632a6fd91b91b47  --control-plane --certificate-key 56baf84218ea805751b5b5c9f1a6c6212f42bb485128fb5b8bedd13cda0359f6
