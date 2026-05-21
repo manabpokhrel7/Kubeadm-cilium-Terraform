043129e5cfca96bf82a9c7b3013d2d7b01d79569
manabpokhrel7 <65856123+manabpokhrel7@users.noreply.github.com>
2026-05-20 21:00:28 -0400

    cicd

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sleep 100
-ansible-playbook automation-setup.yml && ansible-playbook control.yml && ansible-playbook worker.yml && ansible-playbook localhost.yml
+ansible-playbook automation-setup.yml
+ansible-playbook control.yml
+ansible-playbook worker.yml
+ansible-playbook localhost.yml
+ansible-playbook crds.yml
