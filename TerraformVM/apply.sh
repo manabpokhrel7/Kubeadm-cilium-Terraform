918dafd96b277fc0a2feaead001e55eefbc12144
manabpokhrel7 <65856123+manabpokhrel7@users.noreply.github.com>
2026-05-26 18:46:27 -0400

    Argocd fix

--- TerraformVM/apply.sh
+++ TerraformVM/apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 terraform import -var-file=variables.tfvars google_compute_address.tcp_lb_ip projects/kubeadm-public-ip-lab/regions/us-west1/addresses/control-tcp-ip
-terraform import -var-file=variables.tfvars google_compute_global_address.lb_ip projects/kubeadm-public-ip-lab/global/addresses/kubernetes-lb-ip
+terraform import -var-file=variables.tfvars google_compute_address.lb_ip projects/kubeadm-public-ip-lab/regions/us-west1/addresses/kubernetes-lb-ip
 terraform apply --auto-approve -var-file=variables.tfvars
 rm -rf output.txt
 terraform output > output.txt
--- TerraformVM/destroy.sh
+++ TerraformVM/destroy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 terraform state rm google_compute_address.tcp_lb_ip
-terraform state rm google_compute_global_address.lb_ip
+terraform state rm google_compute_address.lb_ip
 terraform destroy --auto-approve -var-file=variables.tfvars
 rm -rf ../inventory
 
