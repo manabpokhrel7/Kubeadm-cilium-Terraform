1d3237aa0901bf95bd583b7405bf616f0656f630
manabpokhrel7 <65856123+manabpokhrel7@users.noreply.github.com>
2026-05-29 16:56:15 -0400

    cicd

--- control_join.sh
+++ control_join.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-kubeadm join api.kubernetes.manabpokhrel.com.np:6443 --token lnwfw9.ezl5d6pisca54v4e --discovery-token-ca-cert-hash sha256:b9174b546d0d227952d7e00b27c092f146bf503f4c9dd038ba3af016deb804e2  --control-plane --certificate-key 97d054efd34b2a1d6b0dec080a3dedf05a55c62c1b6c7f66a4654bc9b0d9a682
+kubeadm join api.kubernetes.manabpokhrel.com.np:6443 --token 1ey255.3jzw8m1qettri4ps --discovery-token-ca-cert-hash sha256:b9174b546d0d227952d7e00b27c092f146bf503f4c9dd038ba3af016deb804e2  --control-plane --certificate-key 43b67cbee45df98b37af2b47cde8758991d2171439c17b7af1b8f4d9744cde95
