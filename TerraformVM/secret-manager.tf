de06f49a1c195b40f977c83032204e0d19c5ac90
manabpokhrel7 <65856123+manabpokhrel7@users.noreply.github.com>
2026-06-10 16:53:45 -0400

    bot cicd trigger

--- TerraformVM/secret-manager.tf
+++ TerraformVM/secret-manager.tf
@@ -18,15 +18,77 @@ resource "google_secret_manager_secret" "secret-basic" {
   deletion_protection = false
 }
 
-resource "google_secret_manager_secret" "openai_secret" {
+resource "google_secret_manager_secret" "openai_api_key" {
   secret_id = "openai-secret"
 
   replication {
     auto {}
   }
+
+  deletion_protection = false
 }
 
-resource "google_secret_manager_secret_version" "openai_secret_version" {
-  secret      = google_secret_manager_secret.openai_secret.id
+resource "google_secret_manager_secret_version" "openai_api_key" {
+  secret      = google_secret_manager_secret.openai_api_key.id
   secret_data = var.openai_api_key
+}
+
+resource "google_secret_manager_secret" "google_client_id" {
+  secret_id = "google-client-id"
+
+  replication {
+    auto {}
+  }
+
+  deletion_protection = false
+}
+
+resource "google_secret_manager_secret_version" "google_client_id" {
+  secret      = google_secret_manager_secret.google_client_id.id
+  secret_data = var.GOOGLE_CLIENT_ID
+}
+
+resource "google_secret_manager_secret" "google_client_secret" {
+  secret_id = "google-client-secret"
+
+  replication {
+    auto {}
+  }
+
+  deletion_protection = false
+}
+
+resource "google_secret_manager_secret_version" "google_client_secret" {
+  secret      = google_secret_manager_secret.google_client_secret.id
+  secret_data = var.GOOGLE_CLIENT_SECRET
+}
+
+resource "google_secret_manager_secret" "token_uri" {
+  secret_id = "token-uri"
+
+  replication {
+    auto {}
+  }
+
+  deletion_protection = false
+}
+
+resource "google_secret_manager_secret_version" "token_uri" {
+  secret      = google_secret_manager_secret.token_uri.id
+  secret_data = var.TOKEN_URI
+}
+
+resource "google_secret_manager_secret" "secret_key" {
+  secret_id = "secret-key"
+
+  replication {
+    auto {}
+  }
+
+  deletion_protection = false
+}
+
+resource "google_secret_manager_secret_version" "secret_key" {
+  secret      = google_secret_manager_secret.secret_key.id
+  secret_data = var.SECRET_KEY
 }
--- TerraformVM/variables.tf
+++ TerraformVM/variables.tf
@@ -7,3 +7,23 @@ variable "openai_api_key" {
   description = "OpenAI secret"
   sensitive   = true
 }
+variable "GOOGLE_CLIENT_ID" {
+  type        = string
+  description = "Google client id"
+  sensitive   = true
+}
+variable "GOOGLE_CLIENT_SECRET" {
+  type        = string
+  description = "Google auth client secret"
+  sensitive   = true
+}
+variable "TOKEN_URI" {
+  type        = string
+  description = "Token URI secret"
+  sensitive   = true
+}
+variable "SECRET_KEY" {
+  type        = string
+  description = "Random key secret"
+  sensitive   = true
+}
