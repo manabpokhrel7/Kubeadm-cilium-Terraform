94569205ecc40f95a69670613a80ea2bd80297ab
manabpokhrel7 <65856123+manabpokhrel7@users.noreply.github.com>
2026-07-17 18:18:28 -0400

    bot cicd trigger

---
+++ TerraformVM/disks.tf
@@ -0,0 +1,28 @@
+# 1. Create 3 separate Google Persistent Disks
+resource "google_compute_disk" "ceph_disks" {
+  count = 3
+  name  = "ceph-disk-${count.index}"
+  type  = "pd-balanced"
+  zone  = "us-west1-a" # Match the zone your VMs are running in
+  size  = 20               # 20 GB per disk is plenty for testing
+}
+
+# 2. Attach each disk to its corresponding VM node
+resource "google_compute_attached_disk" "attach_ceph_disks1" {
+  disk        = google_compute_disk.ceph_disks[1].id
+  instance    = "worker1" # Replace with your actual VM resource name reference
+  zone         = "us-west1-a"
+  device_name = "ceph-device"
+}
+resource "google_compute_attached_disk" "attach_ceph_disks2" {
+  disk        = google_compute_disk.ceph_disks[0].id
+  instance    = "worker2" # Replace with your actual VM resource name reference
+  zone         = "us-west1-a"
+  device_name = "ceph-device"
+}
+resource "google_compute_attached_disk" "attach_ceph_disks3" {
+  disk        = google_compute_disk.ceph_disks[2].id
+  instance    = "worker3" # Replace with your actual VM resource name reference
+  zone         = "us-west1-a"
+  device_name = "ceph-device"
+}
