29e40300226c6665c51b6802b9a7b3b7735028ec
manabpokhrel7 <65856123+manabpokhrel7@users.noreply.github.com>
2026-09-12 13:25:11 -0400

    bot cicd trigger

--- control_join.sh
+++ control_join.sh
@@ -1,2 +1,2 @@
 #!/bin/bash 
-kubeadm join api.kubernetes.manabpokhrel.com.np:6443 --token 0bhdfe.wco7dkmstmz8dshp --discovery-token-ca-cert-hash sha256:4ed3f114f8c9dc5d81f361fe019d014655c61deff6a363f556326dad3dffd340  --control-plane --certificate-key 61937b82f8d567741229ca66a1622653088a71c02f24e1aacee7fd64f2ffae79
+kubeadm join api.kubernetes.manabpokhrel.com.np:6443 --token n7xpmh.mfi72utb5vcpid87 --discovery-token-ca-cert-hash sha256:ca99883f40c54ff96cb483acaa7197b07c961c272220c3c5896bcea2d9ead9b8  --control-plane --certificate-key 2170756fc9c95c9f3a9c2642bff18570f733dad222f2b2393a590b2358ed244b
